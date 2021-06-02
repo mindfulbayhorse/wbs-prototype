69846f286c8b10407cf46ec98296ec13b424d3d6
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-06-02 22:58:30 +0300

    React component tests for learning

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,9 @@ import 'bootstrap';
 import '../sass/input.sass';
 import '../scss/input.scss';
 
+let React = require('react');
+let ReactDOM = require('react-dom');
+
 import {dayMonth, Calendar} from '../calendar/app.js';
 
 let datePicker = new Calendar();
@@ -13,4 +16,21 @@ let calendars = document.querySelectorAll(".calendar");
 
 for (let calendar of calendars) {
   calendar.appendChild(datePicker.widget);
+}
+
+import {WBS} from '../js/wbs-widget/components/wbs.js';
+import {Table} from '../js/wbs-widget/components/table.js';
+import {Deliverable} from '../js/wbs-widget/components/deliverable.js';
+
+ReactDOM.render(
+  <Table />,
+  document.getElementById('root')
+);
+
+function testDeliverables(){
+  const deliverables = [
+    "Content", "Design", "Development"
+  ];
+  
+  return deliverables;
 }
