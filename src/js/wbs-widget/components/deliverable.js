8629ce4e51c60c6900094263278fdb392612eaed
Olga Zhilkova <mindfulbayhorse@gmail.com>
2021-06-06 19:44:38 +0300

    title of the deliverable can be changed by user

--- src/js/wbs-widget/components/deliverable.js
+++ src/js/wbs-widget/components/deliverable.js
@@ -1,11 +1,20 @@
 let React = require('react');
 
 export class Deliverable extends React.Component { 
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      'title': ''
+    }
+  }
   render(){
     
     return (
       <tr>
-        <td>{this.props.title}</td>
+        <td><input title={this.props.title} 
+          value={this.props.title} 
+          onChange={()=>{this.setState({'title': event.target.value})}} /></td>
       </tr>);
   }
 }
